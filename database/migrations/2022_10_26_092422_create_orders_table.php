f68ce968945057db0a97ed2588107ac79e898042
Fathan Asyhari <fathan.asyhari@gmail.com>
2022-10-28 09:11:31 +0700

    membuat model dan migrasi orders dan mengubah migrasi user

--- database/migrations/2022_10_26_092422_create_orders_table.php
+++ database/migrations/2022_10_26_092422_create_orders_table.php
@@ -13,14 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('phone_number')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('amount');
             $table->timestamps();
         });
     }
@@ -32,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('orders');
     }
 };
